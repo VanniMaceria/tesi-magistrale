7993b51952d1d877c57df21852c1cbab421ef5dd
VanniMaceria <andreabucci123@gmail.com>
2026-03-16 15:29:41 +0100

    Cambio dataset in MNIST

--- baseline_flower/quickstart-pytorch/pytorchexample/task.py
+++ baseline_flower/quickstart-pytorch/pytorchexample/task.py
@@ -15,17 +15,20 @@ class Net(nn.Module):
 
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
+        # MNIST uses 1 channel rather than 3 (RGB) 
+        self.conv1 = nn.Conv2d(1, 6, 5) 
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        
+        # Per MNIST 28x28 -> Conv1 -> Pool -> Conv2 -> Pool la dimensione finale è 16 * 4 * 4
+        self.fc1 = nn.Linear(16 * 4 * 4, 120) 
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
+        x = x.view(-1, 16 * 4 * 4) 
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         return self.fc3(x)
@@ -33,7 +36,7 @@ class Net(nn.Module):
 
 fds = None  # Cache FederatedDataset
 
-pytorch_transforms = Compose([ToTensor(), Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+pytorch_transforms = Compose([ToTensor(), Normalize((0.1307,), (0.3081,))])
 
 
 def apply_transforms(batch):
@@ -43,16 +46,18 @@ def apply_transforms(batch):
 
 
 def load_data(partition_id: int, num_partitions: int, batch_size: int):
-    """Load partition CIFAR10 data."""
+    """Load partition ylecun/mnist data for clients."""
     # Only initialize `FederatedDataset` once
     global fds
     if fds is None:
         partitioner = IidPartitioner(num_partitions=num_partitions)
         fds = FederatedDataset(
-            dataset="uoft-cs/cifar10",
+            dataset="ylecun/mnist",
             partitioners={"train": partitioner},
         )
     partition = fds.load_partition(partition_id)
+    #MNIST dataset has "image" column, but our model expects "img" column, so we rename it here
+    partition = partition.rename_column("image", "img")
     # Divide data on each node: 80% train, 20% test
     partition_train_test = partition.train_test_split(test_size=0.2, seed=42)
     # Construct dataloaders
@@ -65,9 +70,9 @@ def load_data(partition_id: int, num_partitions: int, batch_size: int):
 
 
 def load_centralized_dataset():
-    """Load test set and return dataloader."""
-    # Load entire test set
-    test_dataset = load_dataset("uoft-cs/cifar10", split="test")
+    """Load the entire test set as a centralized dataset for evaluation on the server"""
+    test_dataset = load_dataset("ylecun/mnist", split="test")
+    test_dataset = test_dataset.rename_column("image", "img")
     dataset = test_dataset.with_format("torch").with_transform(apply_transforms)
     return DataLoader(dataset, batch_size=128)
 
